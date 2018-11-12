03c4efb51189f522a5071908c2389293919f6480
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-11-11 20:30:10 -0500

    added missing encoder/decoder implementation

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bx
-    URL https://github.com/mchiasson/bx/archive/v1.0.0-p1.tar.gz
-    SHA1 0f16cf582faa091165941697cf3499b06af0318e
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p3.tar.gz
+    SHA1 a3aa9a6d0301651459fad2fed61e4827722d2bb6
 )
