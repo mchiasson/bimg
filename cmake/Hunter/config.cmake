14880c676c25e7776b329c1adde9fba4accafa4f
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-12-08 15:08:08 -0500

    bimg upgraded to C++14 and using latest bx

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bx
-    URL https://github.com/mchiasson/bx/archive/v1.0.0-p3.tar.gz
-    SHA1 a3aa9a6d0301651459fad2fed61e4827722d2bb6
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p4.tar.gz
+    SHA1 294ea7afd7b4470613451ffb6e502cab5cc87e4c
 )
