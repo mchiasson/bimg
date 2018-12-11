e08f634f4015dc639f17c7eaf11f49b41c5da6bc
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-12-11 18:44:13 -0500

    added texturec support and moved namespace to bkaradzic::

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bx
-    URL https://github.com/mchiasson/bx/archive/v1.0.0-p4.tar.gz
-    SHA1 294ea7afd7b4470613451ffb6e502cab5cc87e4c
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p6.tar.gz
+    SHA1 37c85f06dcc185b9bdc6d1b8f201423c7b1022bb
 )
