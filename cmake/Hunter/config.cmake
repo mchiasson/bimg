8b24b0236c1cec34adffcf853392ad0e1fe6c3a5
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2018-12-17 21:25:36 -0500

    updated to the latest bx

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bx
-    URL https://github.com/mchiasson/bx/archive/v1.0.0-p7.tar.gz
-    SHA1 e8735ca5ca598cbce9afec747b205c47ca627836
+    URL https://github.com/mchiasson/bx/archive/v1.0.0-p8.tar.gz
+    SHA1 1884503ecfe6570814d191e5176fccc44e16db60
 )
