70e09ba6a9aee8b6fb7b0943c5b518be93cc899f
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-02-21 17:25:32 -0500

    pointing to latest bx release

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bx
-    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-youi-1.tar.gz
-    SHA1 1dcecc9347d3216c0103ddbc821c29453be6f0ce
+    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-youi-2.tar.gz
+    SHA1 42e1587395798c13e440da2437a32bcba701e7c8
 )
